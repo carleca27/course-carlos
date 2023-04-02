385a3257c1773fc11a699812a5d7e6eeb65b8ba1
Javier Díaz <javier.diaz.jimenez@vicoland.com>
2023-04-02 23:32:05 +0200

    Exercise 01 completed

--- exercises/01/cuentapalabras.js
+++ exercises/01/cuentapalabras.js
@@ -1,94 +1,106 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Listo para el combate")
+
+    // Initialize button
+    enableDisableButton();
 })
 
-cuenta_cosas.disabled = true;
-
 area_de_texto.addEventListener("change", (e) => {
-    if (area_de_texto.value.length > 0) {
-        cuenta_cosas.disabled = false;
-    }
-    else {
-        cuenta_cosas.disabled = true;
-    }
+    enableDisableButton();
 
     e.preventDefault();
 })
 
 ///////////////////////////////////////////////////////////////////////////
 
+function enableDisableButton()
+{
+    console.log('Text size: ' + area_de_texto.value.length);
+    cuenta_cosas.disabled = area_de_texto.value.length === 0;
+}
+
+function cuenta_consonantes()
+{
+    var consonantes = /[bcdfghjklmnñpqrstvwxyzBCDFGHJKLMNÑPQRSTVWXYZ]/g
+    consonantes_encontradas = area_de_texto.value.match(consonantes)
+    if (consonantes_encontradas==null) {
+        return 0
+    } else {
+        return consonantes_encontradas.length
+    }
+}
+
+function cuenta_vocales()
+{
+    var vocales = /[aeiouAEIOUáéíóúÁÉÍÓÚ]/g //Dato para mí: si añado un "^" delante de la letra, por algún motivo toma la mayúscula como minúscula y viceversa.
+    var vocales_encontradas = area_de_texto.value.match(vocales)
+    if (vocales_encontradas==null) {
+        return 0
+    } else {
+        return vocales_encontradas.length
+    }
+}
+
+function mostRepeatedWords(texto)
+{
+    // Obtain an array of words from the text
+    let words = texto.match(/\w+/g); // Si en vez de /\w+/g pusiera /\w/g se contarían los caracteres alfanuméricos uno a uno, en vez de las palabras.
+    console.log(words);
+
+    // Calculate the frequency of words, create an array of objects
+    console.log('-- calc frequency')
+    let wordFrequency = [];
+
+    for (let word of words) {
+        // Check if the word was added
+        let wordObj = wordFrequency.find(wordObj => wordObj.name === word);
+
+        if (wordObj === undefined) {
+            // Insert new word obj
+            let wordObj = {"name": word, counter: 1};
+            wordFrequency.push(wordObj);
+        } else {
+            // Increase counter
+            wordObj.counter++;
+        }
+    }
+
+    console.log(wordFrequency)
+
+    console.log('-- start sorting')
+
+    // Sort by most frequent to less frequent
+    let sortedWords = wordFrequency.sort((wordA, wordB) => (wordA.counter < wordB.counter) ? 1: -1);
+
+    console.log(sortedWords)
+
+    // Now get the 3 most frequents
+    console.log('-- most frequents')
+    let mostFrequents = sortedWords.slice(0, 3);
+    console.log(mostFrequents)
+
+    let frequentsName = mostFrequents.map(word => {return word.name});
+    console.log(frequentsName)
+
+    return frequentsName.join(', ');
+
+    // Lo anterior se podría concatenar tal que así:
+    //return sortedWords.slice(0, 3).map(word => {return word.name}).join(', ');
+}
+
 cuenta_cosas.addEventListener("click", () => {
-    var numero_letras = cuenta_consonantes() + cuenta_vocales()
+    let numero_letras = cuenta_consonantes() + cuenta_vocales()
     let por_ciento_vocales = (cuenta_vocales() * 100) / numero_letras
+    let palabras = mostRepeatedWords(area_de_texto.value);
 
     consonantes_totales.textContent = "Número total de consonantes = " + cuenta_consonantes()
     vocales_totales.textContent = "Número total de vocales = " + cuenta_vocales()
     total_caracteres.textContent = "Número total de caracteres = " + area_de_texto.value.length
     porcentaje_vocales.textContent = "Porcentaje de vocales = " + por_ciento_vocales +"%"
+    palabras_repetidas.textContent = "Palabras más repetidas = " + palabras
+
     /* Aviso a navegante: En efecto, si tienes la desfachatez de poner en el area_de_texto algo como una
     "ü", el % que saldrá es NaN. Estuve intentando cambiarlo un ratillo para que ahí saliera también 0.
     No fui capaz en el tiempo que le dediqué, y no quise echarle más rato. Pero si se me dice cómo se hacía. */
 
-function cuenta_consonantes(){
-  var consonantes = /[bcdfghjklmnñpqrstvwxyzBCDFGHJKLMNÑPQRSTVWXYZ]/g
-  consonantes_encontradas = area_de_texto.value.match(consonantes)
-  if (consonantes_encontradas==null){return 0} else {return consonantes_encontradas.length}
-}
-
-function cuenta_vocales(){
-  var vocales = /[aeiouAEIOUáéíóúÁÉÍÓÚ]/g //Dato para mí: si añado un "^" delante de la letra, por algún motivo toma la mayúscula como minúscula y viceversa.
-  var vocales_encontradas = area_de_texto.value.match(vocales)
-  if (vocales_encontradas==null){return 0} else {return vocales_encontradas.length}
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////
-
-// ÉSTE ES EL CÓDIGO QUE PRETENDÍA CONTAR PALABRAS:
-
-    // let string = area_de_texto.value
-    texto = area_de_texto.value
-
-    // function findMostRepeatedWord(string){
-    function palabra_repetida(){
-      // let words = string.match(/\w+/g);
-      var conteo_palabras = texto.match(/\w+/g); // Si en vez de /\w+/g pusiera /\w/g se contarían los caracteres alfanuméricos uno a uno, en vez de las palabras.
-      // console.log(words);
-      console.log(conteo_palabras);
-
-      // let ocurrencias = {};
-      var caso_palabra = {};
-      
-      // for (let word of words) {
-      for (let word of conteo_palabras){
-        // if (ocurrencias[word]) {ocurrencias[word]++;} 
-        if (caso_palabra[word]) {caso_palabra[word]++}
-        // else {ocurrencias [word] = 1;}}
-        else {caso_palabra[word] = 1;}
-      }
-      // console.log (ocurrencias)
-      console.log(caso_palabra)
-
-      // let max = 0;
-      var palabra_primera = 0;
-      var palabra_segunda = 0
-      var palabra_tercera = 0
-      // let mostRepeatedWord = '';
-      var pal1 = '';
-      var pal2 = ''
-      var pal3 = ''
-      // for (let word of words){
-      for (let word of conteo_palabras){
-        // if(ocurrencias[word] > max) {max = ocurrencias[word]; mostRepeatedWord = word};}
-        if(caso_palabra[word] > palabra_primera){palabra_primera = caso_palabra[word]; pal1 = word}
-        else if (palabra_primera > caso_palabra[word] > palabra_segunda)
-          {palabra_segunda = caso_palabra[word]; pal2 = word}
-        else if (palabra_segunda > caso_palabra[word] > palabra_tercera)
-          {palabra_tercera = caso_palabra[word]; pal3 = word}
-      }
-
-      // return mostRepeatedWord
-      return {pal1, pal2, pal3}
-    }
-
-    console.log(palabra_repetida());   
 })
